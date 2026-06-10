253a230e69b81a3c6f5565db337e27e6b0404b0a
brandonjimenez21 <brandonjimenez.dev@gmail.com>
2026-06-10 17:44:29 -0500

    style: format code with go fmt

--- internal/api/client.go
+++ internal/api/client.go
@@ -18,13 +18,13 @@ const (
 )
 
 type openWeatherResponse struct {
-	Name string `json:"name"`
-	Dt   int64  `json:"dt"`
+	Name  string `json:"name"`
+	Dt    int64  `json:"dt"`
 	Coord struct {
 		Lat float64 `json:"lat"`
 		Lon float64 `json:"lon"`
 	} `json:"coord"`
-	Sys  struct {
+	Sys struct {
 		Sunrise int64 `json:"sunrise"`
 		Sunset  int64 `json:"sunset"`
 	} `json:"sys"`
@@ -44,7 +44,7 @@ type openWeatherResponse struct {
 
 type forecastResponse struct {
 	List []struct {
-		Dt   int64 `json:"dt"`
+		Dt   int64   `json:"dt"`
 		Pop  float64 `json:"pop"`
 		Main struct {
 			Temp float64 `json:"temp"`
@@ -111,7 +111,9 @@ func (c *Client) GetWeather(city string) (*model.Weather, error) {
 				rainProb = int(fData.List[0].Pop * 100)
 			}
 			for i, item := range fData.List {
-				if i >= 8 { break }
+				if i >= 8 {
+					break
+				}
 				t := time.Unix(item.Dt, 0)
 				forecastPoints = append(forecastPoints, model.ForecastPoint{
 					Time: t.Format("15:04"),
@@ -125,25 +127,25 @@ func (c *Client) GetWeather(city string) (*model.Weather, error) {
 	isNight := data.Dt < data.Sys.Sunrise || data.Dt > data.Sys.Sunset
 
 	// Golden hour: within 60 minutes of sunrise or sunset
-	const goldenHourLimit = 3600 
+	const goldenHourLimit = 3600
 	isGoldenHour := (data.Dt >= data.Sys.Sunrise-goldenHourLimit && data.Dt <= data.Sys.Sunrise+goldenHourLimit) ||
 		(data.Dt >= data.Sys.Sunset-goldenHourLimit && data.Dt <= data.Sys.Sunset+goldenHourLimit)
 
 	weather := &model.Weather{
-		City:        data.Name,
-		Temperature: data.Main.Temp,
-		FeelsLike:   data.Main.FeelsLike,
-		Humidity:    data.Main.Humidity,
-		Description: data.Weather[0].Description,
-		Condition:   data.Weather[0].Main,
-		WindSpeed:   data.Wind.Speed,
-		Forecast:    forecastPoints,
-		IsNight:     isNight,
+		City:         data.Name,
+		Temperature:  data.Main.Temp,
+		FeelsLike:    data.Main.FeelsLike,
+		Humidity:     data.Main.Humidity,
+		Description:  data.Weather[0].Description,
+		Condition:    data.Weather[0].Main,
+		WindSpeed:    data.Wind.Speed,
+		Forecast:     forecastPoints,
+		IsNight:      isNight,
 		IsGoldenHour: isGoldenHour,
-		Sunrise:     time.Unix(data.Sys.Sunrise, 0).Format("15:04"),
-		Sunset:      time.Unix(data.Sys.Sunset, 0).Format("15:04"),
-		RainProb:    rainProb,
-		UVIndex:     0,
+		Sunrise:      time.Unix(data.Sys.Sunrise, 0).Format("15:04"),
+		Sunset:       time.Unix(data.Sys.Sunset, 0).Format("15:04"),
+		RainProb:     rainProb,
+		UVIndex:      0,
 	}
 
 	weather.CalculateThermalSensation()
